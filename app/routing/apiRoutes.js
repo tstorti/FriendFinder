c9d305e4b1a43b6756ace514e8491be8b4f3870a
Tony Storti <tonystorti@gmail.com>
2017-08-07 11:23:33 -0500

    initial setup for apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,19 @@
 // API Routes
 // =============================================================
 
-
-// 4. Your `apiRoutes.js` file should contain two routes:
-
-//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+var friendData = require("../data/friends.js");
 
 module.exports = function(app) {
 
+//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+    });
+
+//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+    app.post("/api/friends", function(req, res) {
+        friendData.push(req.body);
+    });
+
 };
+
