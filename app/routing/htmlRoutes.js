67c155580593fff9a986c68b3a5e7ca722bb49dd
Tony Storti <tonystorti@gmail.com>
2017-08-07 11:17:16 -0500

    initial setup for htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,18 @@
 // HTML Routes
 // =============================================================
 
-// 3. Your `htmlRoutes.js` file should include two routes:
-
-//    * A GET Route to `/survey` which should display the survey page.
-//    * A default USE route that leads to `home.html` which displays the home page. 
-
 var path = require("path");
 
 module.exports = function(app) {
-
+    
+//    * A GET Route to `/survey` which should display the survey page.    
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    
+//    * A default USE route that leads to `home.html` which displays the home page. 
+    app.use(function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    
 };
