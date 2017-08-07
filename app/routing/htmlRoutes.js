b386d5fb1bd8eaeb5c6bdf11c51c6daad4c30485
Tony Storti <tonystorti@gmail.com>
2017-08-07 10:58:37 -0500

    initial commit of file structure

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,7 @@
+// HTML Routes
+// =============================================================
+
+// 3. Your `htmlRoutes.js` file should include two routes:
+
+//    * A GET Route to `/survey` which should display the survey page.
+//    * A default USE route that leads to `home.html` which displays the home page. 
